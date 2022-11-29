951645e2a0d57b5c58f4786d69000e93675cbd5f
王佳 <792836909@qq.com>
2022-11-30 00:59:12 +0800

    开坑检索树服务

---
+++ src/main/java/jmw/RegressionSearchTree.java
@@ -0,0 +1,16 @@
+package jmw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jmwang-code
+ */
+@SpringBootApplication
+public class RegressionSearchTree {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegressionSearchTree.class, args);
+    }
+
+}
