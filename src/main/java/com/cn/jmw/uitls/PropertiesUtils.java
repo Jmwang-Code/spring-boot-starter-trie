32fdecf572572fcc331c534c06e4fd8e70e64265
王佳 <792836909@qq.com>
2022-12-07 21:17:24 +0800

    核心算法：配置文件Properties解析

--- src/main/java/com/cn/jmw/uitls/PropertiesUtils.java
+++ src/main/java/com/cn/jmw/uitls/PropertiesUtils.java
@@ -1,5 +1,8 @@
 package com.cn.jmw.uitls;
 
+import java.io.*;
+import java.util.Properties;
+
 /**
  * @author jmw
  * @Description I want to define a loading mode.For example,reflect and annotate to load a file.
@@ -11,4 +14,61 @@ package com.cn.jmw.uitls;
  *
  */
 public class PropertiesUtils {
+
+    PropertiesUtils(){}
+
+    public static PropertiesUtils.Builder builder() {
+        return new PropertiesUtils.Builder();
+    }
+
+    public static void main(String[] args) {
+        Properties build = PropertiesUtils.builder().file("C:\\Users\\jmw\\Desktop\\jmw-code\\regression-search-tree\\src\\main\\resources\\hello.properties").build();
+        System.out.println();
+    }
+
+    public static class Builder<T>{
+
+        private T file;
+
+        Builder(){}
+
+        public PropertiesUtils.Builder file(T file) {
+            this.file = file;
+            return this;
+        }
+
+        public Properties build() {
+            Properties properties = null;
+            if (file instanceof File){
+                properties = this.xmlToProperties((File) file);
+            }else if (file instanceof String){
+                properties = this.xmlToProperties((String) file);
+            }else {
+                throw new PropertiesIsNullException("properties is null");
+            }
+            return properties;
+        }
+
+
+        private Properties xmlToProperties(File file) {
+            Properties properties = new Properties();
+            try {
+                properties.load(new BufferedReader(new FileReader(file)));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            return properties;
+        }
+
+        private Properties xmlToProperties(String file) {
+            Properties properties = new Properties();
+            try {
+                properties.load(new BufferedReader(new FileReader(file)));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            return properties;
+        }
+
+    }
 }
