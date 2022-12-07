374cc722c5f83140e23d92e32041828ec30448b4
王佳 <792836909@qq.com>
2022-12-07 15:25:34 +0800

    核心算法：配置文件解析

---
+++ src/main/java/com/cn/jmw/uitls/PropertiesUtils.java
@@ -0,0 +1,14 @@
+package com.cn.jmw.uitls;
+
+/**
+ * @author jmw
+ * @Description I want to define a loading mode.For example,reflect and annotate to load a file.
+ * @date 2022年12月06日 23:56
+ * @Version 1.0
+ *
+ * @commonCongraration("Files")
+ * private Configuration configuration;
+ *
+ */
+public class PropertiesUtils {
+}
---
+++ src/main/java/com/cn/jmw/uitls/XmlUtils.java
@@ -0,0 +1,10 @@
+package com.cn.jmw.uitls;
+
+/**
+ * @author jmw
+ * @Description TODO
+ * @date 2022年12月06日 23:56
+ * @Version 1.0
+ */
+public class XmlUtils {
+}
