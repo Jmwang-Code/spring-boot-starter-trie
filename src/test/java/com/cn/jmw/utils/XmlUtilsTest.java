54de3868e767f329f3bd5be7deaac0f56aba1daa
王佳 <792836909@qq.com>
2022-12-09 18:24:03 +0800

    核心算法：配置文件测试模块

---
+++ src/test/java/com/cn/jmw/utils/XmlUtilsTest.java
@@ -0,0 +1,33 @@
+package com.cn.jmw.utils;
+
+import com.alibaba.fastjson.JSONObject;
+import com.cn.jmw.entity.config.TrieConfig;
+import com.cn.jmw.trie.TrieNode;
+import com.cn.jmw.uitls.XmlUtils;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author jmw
+ * @Description TODO
+ * @date 2022年12月09日 17:33
+ * @Version 1.0
+ */
+public class XmlUtilsTest {
+    @Test
+    public void test() {
+        JSONObject jsonObject = null;
+        try {
+            jsonObject = XmlUtils.xmlToJson("C:\\Users\\jmw\\Desktop\\jmw-code\\regression-search-tree\\src\\main\\resources\\triedata-config.xml");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(jsonObject);
+
+        TrieConfig trieConfig = XmlUtils.XmlToObject(TrieConfig.class, "/triedata-config.xml");
+        System.out.println(trieConfig);
+
+        TrieNode trieNode = XmlUtils.XmlToObject(TrieNode.class, "/triedata-config.xml");
+        System.out.println(trieNode);
+
+    }
+}
