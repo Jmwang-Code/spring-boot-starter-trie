c4c81f98ca7f1a2130e87c1e91fc77743d3a0aa2
王佳 <792836909@qq.com>
2023-01-16 22:35:58 +0800

    核心算法：返回结果保存

---
+++ src/main/java/com/cn/jmw/trie/en/Result.java
@@ -0,0 +1,20 @@
+package com.cn.jmw.trie.en;
+
+/**
+ * @author jmw
+ * @Description 结果集
+ * @date 2023年01月16日 20:20
+ * @Version 1.0
+ */
+public class Result<T> {
+
+    T value;
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+}
