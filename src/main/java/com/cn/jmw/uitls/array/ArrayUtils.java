13bcd40adb8153d6ed77cc8f4203e4227b0dd8b7
王佳 <792836909@qq.com>
2023-02-06 13:56:03 +0800

    核心算法：预备算法库，Array原地翻转

---
+++ src/main/java/com/cn/jmw/uitls/array/ArrayUtils.java
@@ -0,0 +1,27 @@
+package com.cn.jmw.uitls.array;
+
+/**
+ * @author jmw
+ * @Description TODO
+ * @date 2023年02月06日 13:52
+ * @Version 1.0
+ */
+public class ArrayUtils {
+
+    /**
+     * @Param []
+     * @return void
+     * @exception
+     * @Date 2023/2/6 13:55
+     * 原地翻转
+     */
+    public void reverse(int[] arr,int start,int end){
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start += 1;
+            end -= 1;
+        }
+    }
+}
