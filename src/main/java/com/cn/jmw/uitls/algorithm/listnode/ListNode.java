43593d35409b8ffd42cd108aa340f81ef1033091
王佳 <792836909@qq.com>
2023-04-02 23:34:11 +0800

    附加功能：

---
+++ src/main/java/com/cn/jmw/uitls/algorithm/listnode/ListNode.java
@@ -0,0 +1,49 @@
+package com.cn.jmw.uitls.algorithm.listnode;
+
+/**
+ * @author jmw
+ * @Description TODO
+ * @date 2023年04月02日 23:15
+ * @Version 1.0
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        // 设置 dummyNode 是这一类问题的一般做法
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+        ListNode pre = dummyNode;
+        for (int i = 0; i < left - 1; i++) {
+            pre = pre.next;
+        }
+        ListNode cur = pre.next;
+        ListNode next;
+        for (int i = 0; i < right - left; i++) {
+            next = cur.next;
+            cur.next = next.next;
+            next.next = pre.next;
+            pre.next = next;
+        }
+        return dummyNode.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode listNode = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+        new ListNode().reverseBetween(listNode,2,4);
+    }
+}
