c5b06e877a888e1514b6d5167f905f89b89bfd66
王佳 <792836909@qq.com>
2023-04-03 18:02:18 +0800

    持久化功能：SQLite demo 本地连接 远程连接代码 内存连接代码 本地连接代码

--- src/main/java/com/cn/jmw/persistence/SqliteDriver.java
+++ src/main/java/com/cn/jmw/persistence/SqliteDriver.java
@@ -32,6 +32,72 @@ public class SqliteDriver {
     public static final String SQLITE_DRIVER_FILE = "org.sqlite.JDBC";
     public static final String SQLITE_URL_FILE = "jdbc:sqlite:" + SQLITE_FILE_PATH;
 
+    //SQLite本地持久化连接代码
+    public static void main(String[] args) throws Exception {
+        Class.forName(SQLITE_DRIVER_FILE);
+        Connection conn = DriverManager.getConnection(SQLITE_URL_FILE);
+        Statement stmt = conn.createStatement();
+        stmt.execute("create table test(id int primary key, name varchar(255))");
+        stmt.execute("insert into test values(1, 'Hello')");
+        stmt.execute("insert into test values(2, 'World')");
+        ResultSet rs = stmt.executeQuery("select * from test");
+        while (rs.next()) {
+            System.out.println(rs.getInt(1) + " " + rs.getString(2));
+        }
+        rs.close();
+        stmt.close();
+        conn.close();
+    }
 
+    //SQLite内存连接代码
+    public static void main1(String[] args) throws Exception {
+        Class.forName(SQLITE_DRIVER_MEMORY);
+        Connection conn = DriverManager.getConnection(SQLITE_URL_MEMORY);
+        Statement stmt = conn.createStatement();
+        stmt.execute("create table test(id int primary key, name varchar(255))");
+        stmt.execute("insert into test values(1, 'Hello')");
+        stmt.execute("insert into test values(2, 'World')");
+        ResultSet rs = stmt.executeQuery("select * from test");
+        while (rs.next()) {
+            System.out.println(rs.getInt(1) + " " + rs.getString(2));
+        }
+        rs.close();
+        stmt.close();
+        conn.close();
+    }
+
+    //SQLite远程连接代码
+    public static void main2(String[] args) throws Exception {
+        Class.forName(SQLITE_DRIVER_REMOTE);
+        Connection conn = DriverManager.getConnection(SQLITE_URL_REMOTE);
+        Statement stmt = conn.createStatement();
+        stmt.execute("create table test(id int primary key, name varchar(255))");
+        stmt.execute("insert into test values(1, 'Hello')");
+        stmt.execute("insert into test values(2, 'World')");
+        ResultSet rs = stmt.executeQuery("select * from test");
+        while (rs.next()) {
+            System.out.println(rs.getInt(1) + " " + rs.getString(2));
+        }
+        rs.close();
+        stmt.close();
+        conn.close();
+    }
+
+    //SQLite本地连接代码
+    public static void main3(String[] args) throws Exception {
+        Class.forName(SQLITE_DRIVER);
+        Connection conn = DriverManager.getConnection(SQLITE_URL);
+        Statement stmt = conn.createStatement();
+        stmt.execute("create table test(id int primary key, name varchar(255))");
+        stmt.execute("insert into test values(1, 'Hello')");
+        stmt.execute("insert into test values(2, 'World')");
+        ResultSet rs = stmt.executeQuery("select * from test");
+        while (rs.next()) {
+            System.out.println(rs.getInt(1) + " " + rs.getString(2));
+        }
+        rs.close();
+        stmt.close();
+        conn.close();
+    }
 
 }
