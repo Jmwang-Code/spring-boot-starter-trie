ce6d667ea2053c3d1120894d69359e6bb1de4380
王佳 <792836909@qq.com>
2023-04-03 18:01:34 +0800

    持久化功能：SQLite 连接方式

--- src/main/java/com/cn/jmw/persistence/SqliteDriver.java
+++ src/main/java/com/cn/jmw/persistence/SqliteDriver.java
@@ -1,5 +1,10 @@
 package com.cn.jmw.persistence;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 /**
  * @author jmw
  * @Description TODO
@@ -7,4 +12,26 @@ package com.cn.jmw.persistence;
  * @Version 1.0
  */
 public class SqliteDriver {
+
+    //SQLite数据库本地连接
+    public static final String SQLITE_DRIVER = "org.sqlite.JDBC";
+    public static final String SQLITE_URL = "jdbc:sqlite:~/test.db";
+
+    //SQLite数据库内存连接
+    public static final String SQLITE_DRIVER_MEMORY = "org.sqlite.JDBC";
+    public static final String SQLITE_URL_MEMORY = "jdbc:sqlite::memory:";
+
+    //SQLite数据库远程连接
+    public static final String SQLITE_DRIVER_REMOTE = "org.sqlite.JDBC";
+    public static final String SQLITE_URL_REMOTE = "jdbc:sqlite://localhost/~/test.db";
+
+    //SQLite生成存储文件的路径
+    public static final String SQLITE_FILE_PATH = "D:\\sqlite\\test.db";
+
+    //SQLite本地连接存储文件
+    public static final String SQLITE_DRIVER_FILE = "org.sqlite.JDBC";
+    public static final String SQLITE_URL_FILE = "jdbc:sqlite:" + SQLITE_FILE_PATH;
+
+
+
 }
