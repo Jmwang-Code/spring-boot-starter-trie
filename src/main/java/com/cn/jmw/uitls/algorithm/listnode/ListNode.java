394832fdba3add8e67921ad57883454e0048c2d2
王佳 <792836909@qq.com>
2023-04-04 11:18:43 +0800

    持久化功能：计算时间复杂度

--- src/main/java/com/cn/jmw/uitls/algorithm/listnode/ListNode.java
+++ src/main/java/com/cn/jmw/uitls/algorithm/listnode/ListNode.java
@@ -42,8 +42,42 @@ public class ListNode {
         return dummyNode.next;
     }
 
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode ans = null;
+        for (int i = 0; i < lists.length; ++i) {
+            ans = mergeTwoLists(ans, lists[i]);
+        }
+        return ans;
+    }
+
+    public ListNode mergeTwoLists(ListNode a, ListNode b) {
+        if (a == null || b == null) {
+            return a != null ? a : b;
+        }
+        ListNode head = new ListNode(0);
+        ListNode tail = head, aPtr = a, bPtr = b;
+        while (aPtr != null && bPtr != null) {
+            if (aPtr.val < bPtr.val) {
+                tail.next = aPtr;
+                aPtr = aPtr.next;
+            } else {
+                tail.next = bPtr;
+                bPtr = bPtr.next;
+            }
+            tail = tail.next;
+        }
+        tail.next = (aPtr != null ? aPtr : bPtr);
+        return head.next;
+    }
+
     public static void main(String[] args) {
         ListNode listNode = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
-        new ListNode().reverseBetween(listNode,2,4);
+        new ListNode().reverseBetween(listNode, 2, 4);
+
+        ListNode listNode1 = new ListNode(1, new ListNode(2, new ListNode(3, null)));
+        ListNode listNode2 = new ListNode(1, new ListNode(1, new ListNode(5, null)));
+        ListNode listNode3 = new ListNode(2, new ListNode(3, null));
+        ListNode[] list = new ListNode[]{listNode1, listNode2, listNode3};
+        System.out.println(new ListNode().mergeKLists(list));
     }
 }
--- src/main/java/com/cn/jmw/uitls/algorithm/twotree/ListNode1.java
+++ src/main/java/com/cn/jmw/uitls/algorithm/twotree/ListNode1.java
@@ -1,48 +1,45 @@
 package com.cn.jmw.uitls.algorithm.twotree;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * @author jmw
  * @Description TODO
  * @date 2023年03月30日 11:15
  * @Version 1.0
  */
-public class ListNode {
+public class ListNode1 {
     int val;
-    ListNode next;
+    ListNode1 next;
 
-    ListNode() {
+    ListNode1() {
     }
 
-    ListNode(int val) {
+    ListNode1(int val) {
         this.val = val;
     }
 
-    ListNode(int val, ListNode next) {
+    ListNode1(int val, ListNode1 next) {
         this.val = val;
         this.next = next;
     }
 
     //删除倒数第N个元素
-    public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode dummy = new ListNode(0, head);
+    public ListNode1 removeNthFromEnd(ListNode1 head, int n) {
+        ListNode1 dummy = new ListNode1(0, head);
         int length = getLength(head);
-        ListNode cur = dummy;
+        ListNode1 cur = dummy;
         for (int i = 1; i < length - n + 1; ++i) {
             cur = cur.next;
         }
         cur.next = cur.next.next;
-        ListNode ans = dummy.next;
+        ListNode1 ans = dummy.next;
         return ans;
     }
 
     //链表经典双指针用法 做差寻找 位移差值
-    public ListNode removeNthFromEnd2(ListNode head, int n) {
-        ListNode dummy = new ListNode(0, head);
-        ListNode first = head;
-        ListNode second = dummy;
+    public ListNode1 removeNthFromEnd2(ListNode1 head, int n) {
+        ListNode1 dummy = new ListNode1(0, head);
+        ListNode1 first = head;
+        ListNode1 second = dummy;
         for (int i = 0; i < n; ++i) {
             first = first.next;
         }
@@ -51,10 +48,10 @@ public class ListNode {
             second = second.next;
         }
         second.next = second.next.next;
-        ListNode ans = dummy.next;
+        ListNode1 ans = dummy.next;
         return ans;
     }
-    public int getLength(ListNode head) {
+    public int getLength(ListNode1 head) {
         int length = 0;
         while (head != null) {
             ++length;
@@ -64,8 +61,8 @@ public class ListNode {
     }
 
     //节点逆转
-    public ListNode nodeReverse(ListNode node){
-        ListNode current = null, prev = null;
+    public ListNode1 nodeReverse(ListNode1 node){
+        ListNode1 current = null, prev = null;
 
         while (node != null) {
             current = node;
@@ -76,12 +73,12 @@ public class ListNode {
         return prev;
     }
 
-    public ListNode nodeReverse2(ListNode head) {
+    public ListNode1 nodeReverse2(ListNode1 head) {
         //记录pre节点和 当前节点
-        ListNode current = head,pre = null;
+        ListNode1 current = head,pre = null;
         while (current!=null){
             //保留下一个节点
-            ListNode next = current.next;
+            ListNode1 next = current.next;
             //连接节点
             current.next = pre;
             //记录下层循环pre值
@@ -93,8 +90,8 @@ public class ListNode {
     }
 
     public static void main(String[] args) {
-        ListNode listNode = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
-        System.out.println(new ListNode().removeNthFromEnd(listNode,2));
-        System.out.println(new ListNode().nodeReverse2(listNode));
+        ListNode1 listNode1 = new ListNode1(1, new ListNode1(2, new ListNode1(3, new ListNode1(4, new ListNode1(5)))));
+        System.out.println(new ListNode1().removeNthFromEnd(listNode1,2));
+        System.out.println(new ListNode1().nodeReverse2(listNode1));
     }
 }
