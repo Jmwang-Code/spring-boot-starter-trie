eac0de1f73bfbba8cc3d09f24340f0c2644f4245
王佳 <792836909@qq.com>
2023-04-20 00:42:55 +0800

    加载模块:日志颜色 8 OR 256

--- jmw-utils/src/main/java/com/cn/jmw/color/ColorEnum8.java
+++ jmw-utils/src/main/java/com/cn/jmw/color/ColorEnum8.java
@@ -1,30 +1,29 @@
 package com.cn.jmw.color;
 
-import lombok.extern.slf4j.Slf4j;
-
 /**
  * @author jmw
  * @Description
  * @date 2023年04月13日 16:58
  * @Version 1.0
  */
-public enum ColorEnum {
+public enum ColorEnum8 {
 
-    RED(31,4),
-    GREEN(32,4),
-    YELLOW(33,4),
-    BLUE(34,4),
-    PURPLE(35,4),
-    CYAN(36,4),
-    GREY(37,4),
-    BLACK(30,4);
+    RED(31, 4),
+    GREEN(32, 4),
+    YELLOW(33, 4),
+    BLUE(34, 4),
+    PURPLE(35, 4),
+    CYAN(36, 4),
+    GREY(37, 4),
+    BLACK(30, 4);
 
-    ColorEnum(){}
+    ColorEnum8() {
+    }
 
     private int color;
     private int fontType;
 
-    ColorEnum(int color, int fontType) {
+    ColorEnum8(int color, int fontType) {
         this.color = color;
         this.fontType = fontType;
     }
@@ -41,19 +40,20 @@ public enum ColorEnum {
         return String.format("\033[%d;%dm%s\033[0m",this.color, this.fontType, content);
     }
 
-    public static ColorEnum getRandomColor(){
+    public static ColorEnum8 getRandomColor(){
         int i = (int) (Math.random() * 8);
-        return ColorEnum.values()[i];
+        return ColorEnum8.values()[i];
     }
+
 
     public static void main(String[] args) {
         for (int i = 0; i <= 15; i++) {
             for (int j = 0; j <= 15; j++) {
                 int number = i * 15 + j;
                 String str = String.format("%3d", number);
-
-                System.out.print("\u001b[48;5;" + number + "m " + str);
-                System.out.print("\u001b[0m");
+//                String.format("\u001b[48;5;m%s\u001b[0m", this.color, str);
+                System.out.print("\u001b[48;5;" + number + "m " + str + "\u001b[0m");
+//                System.out.print();
             }
             System.out.println();
         }
