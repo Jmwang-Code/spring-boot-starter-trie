f2616cffb1e545c4c7ec970cad59730383d6e8fb
王佳 <792836909@qq.com>
2023-04-21 21:41:24 +0800

    加载模块: 配置类属性增加

--- data-provider/data-provider-base/src/main/java/com/cn/jmw/entity/ProviderEntity.java
+++ data-provider/data-provider-base/src/main/java/com/cn/jmw/entity/ProviderEntity.java
@@ -5,6 +5,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 
 /**
@@ -22,11 +26,13 @@ public class ProviderEntity {
     private String source;
     private List<DataSource> dataSources;
     private int runnableThreadNum = 4;
+    private String persistencePath = System.getProperty("user.dir").replace("\\","/")+ "/local/sqlite/text.cache";
 
     @Data
     public static class SqlQenerator {
 
         //TODO SQL方言生成器，不准备填坑，方言太多，不想写，懒得写，还得学我想躺平
     }
+
 
 }
