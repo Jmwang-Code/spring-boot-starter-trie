69be5e92f37acb7e8824d467c5c81e8828478689
王佳 <792836909@qq.com>
2023-04-21 21:41:04 +0800

    加载模块: 持久化模块

---
+++ persistence/src/main/java/com/cn/jmw/pojo/NodeTable.java
@@ -0,0 +1,14 @@
+package com.cn.jmw.pojo;
+
+/**
+ * @author jmw
+ * @Description H2存储节点表
+ * @date 2023年04月21日 16:50
+ * @Version 1.0
+ */
+public class NodeTable {
+
+    private String str;
+
+    private String code;
+}
