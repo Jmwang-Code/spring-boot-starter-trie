ac4331d340df179f58deed4f7e888e73227d428c
王佳 <792836909@qq.com>
2023-04-28 18:11:03 +0800

    练习: Tree算法优化

--- Trie/src/main/java/com/cn/jmw/uitls/algorithm/dfsbfs/Tree.java
+++ Trie/src/main/java/com/cn/jmw/uitls/algorithm/dfsbfs/Tree.java
@@ -75,11 +75,12 @@ public class Tree {
             pre.next = null;
         }
         return temp;
+    }
 
     //深度优先
     public int maxDepth(TreeNode root) {
-        if (root==null)return 0;
-        return Math.max(maxDepth(root.left), maxDepth(root.right))+1;
+        if (root == null) return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
 
     public static void main(String[] args) {
@@ -127,5 +128,8 @@ class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
-    TreeNode(int x) { val = x; }
+
+    TreeNode(int x) {
+        val = x;
+    }
 }
