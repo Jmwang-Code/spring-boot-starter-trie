3d039194cd09ded360b7397aa51154d9e140e1e4
王佳 <792836909@qq.com>
2023-04-28 18:06:21 +0800

    加载模块: 适配器工厂优化

--- data-provider/data-provider-jdbc/src/main/java/com/cn/jmw/adapter/AdapterFactory.java
+++ data-provider/data-provider-jdbc/src/main/java/com/cn/jmw/adapter/AdapterFactory.java
@@ -3,6 +3,8 @@ package com.cn.jmw.adapter;
 import com.cn.jmw.AdapterEnum;
 import com.cn.jmw.color.ThreadColor;
 import com.cn.jmw.entity.DataSource;
+import com.cn.jmw.trie.Tire;
+import com.cn.jmw.trie.TrieNode;
 import lombok.extern.slf4j.Slf4j;
 
 import java.lang.reflect.InvocationTargetException;
@@ -15,15 +17,13 @@ import java.lang.reflect.InvocationTargetException;
 @Slf4j
 public class AdapterFactory {
     //这里快速创建一个适配器，根据数据源类型，创建对应的适配器
-    public static Adapter<Boolean> createDataAdapter(DataSource dataSource) {
+    public static Adapter<Boolean> createDataAdapter(DataSource dataSource, Tire trieNode) {
         AdapterEnum adapterEnum = AdapterEnum.getAdapterEnum(dataSource.getType());
         log.info(ThreadColor.getColor256(Thread.currentThread().getName()).getColoredString(Thread.currentThread().getName()+"——创建适配器:"+adapterEnum));
         try {
             Class<?> aClass = Class.forName(adapterEnum.getClassName());
-            Object jdbcAdapter = aClass.getDeclaredConstructor().newInstance();
-            JdbcAdapter jd = jdbcAdapter instanceof Adapter ? (JdbcAdapter) jdbcAdapter : null;
-            jd.init(dataSource);
-            return jd;
+            Object jdbcAdapter = aClass.getDeclaredConstructor(DataSource.class,Tire.class).newInstance(dataSource,trieNode);
+            return jdbcAdapter instanceof Adapter ? (JdbcAdapter) jdbcAdapter : null;
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         } catch (InvocationTargetException e) {
@@ -36,4 +36,5 @@ public class AdapterFactory {
             throw new RuntimeException(e);
         }
     }
+
 }
